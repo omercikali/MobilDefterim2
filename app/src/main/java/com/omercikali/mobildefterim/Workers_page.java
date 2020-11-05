c8a51f446a8822c2d1cfc6ef66c1431bed5f9e63
omercikali <ali.omercik1@gmail.com>
2020-11-05 21:37:40 +0300

    m

--- app/src/main/java/com/omercikali/mobildefterim/Workers_page.java
+++ app/src/main/java/com/omercikali/mobildefterim/Workers_page.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -13,49 +14,33 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Workers_page extends AppCompatActivity {
-    private FirebaseDatabase database;
-    private DatabaseReference mref;
-    private Button button;
-    private FirebaseAuth auth;
-
+ImageView add_worker,calendarIm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workers_page);
 
-        auth=FirebaseAuth.getInstance();
+        add_worker=findViewById(R.id.add_worker);
+        calendarIm=findViewById(R.id.calendar_im);
 
-        button=findViewById(R.id.addrecord);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseUser firebaseUser=auth.getCurrentUser();
-                String useremail=firebaseUser.getEmail();
-             useremail=   useremail.replace(".","!");
-                System.out.println(useremail);
 
-                database = FirebaseDatabase.getInstance();
-                mref = database.getReference(useremail).child("kişiler");
-                Isciler isci = new Isciler("", "ali", "20");
-                Isciler isci2 = new Isciler("", "mehmet", "50");
-                Isciler isci3 = new Isciler("", "kerem", "21");
-                Isciler isci4 = new Isciler("", "bilal", "40");
-                Isciler isci5 = new Isciler("", "hasan", "27");
-                Isciler isci6 = new Isciler("", "muzaffer", "30");
-                Isciler isci7 = new Isciler("", "talip", "25");
-                mref.push().setValue(isci);
-                mref.push().setValue(isci2);
-                mref.push().setValue(isci3);
-                mref.push().setValue(isci4);
-                mref.push().setValue(isci5);
-                mref.push().setValue(isci6);
-                mref.push().setValue(isci7);
-            }
-        });
 
 
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     @Override
     public void onBackPressed() {
